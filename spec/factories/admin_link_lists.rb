19d3de13391fd05289ab1083912fa3f1f1550537
balanceM <286515744@qq.com>
2018-10-12 14:23:12 +0800

    factory_girl to factory_bot

--- spec/factories/admin_link_lists.rb
+++ spec/factories/admin_link_lists.rb
@@ -1,8 +1,5 @@
-# FactoryGirl.define do
-#   factory :admin_link_list, class: 'Admin::LinkList' do
+FactoryBot.define do
+  factory :admin_link_list, class: 'Admin::LinkList' do
     
-#   end
-# end
-FactoryGirl.define :link_list do |f|
-  f.title "Mystring"
+  end
 end
